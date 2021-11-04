09d92b81ff8a8ab658266ccb4f1f9c3bc6feb5e1
kyle thayer <kyle.thayer@gmail.com>
2021-11-03 23:21:44 -0700

    Finish demo-2-auth

--- 11 lecture/demo-2-auth/app.js
+++ 11 lecture/demo-2-auth/app.js
@@ -3,6 +3,20 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import sessions from 'express-session';
+import MsIdExpress from 'microsoft-identity-express'
+
+const appSettings = {
+    appCredentials: {
+        clientId: "CLIENT_ID", // Application (client) ID on Azure AD
+        tenantId: "TENANT_ID", // alt. "common" "organizations" "consumers"
+        clientSecret: "CLIENT_SECRET" // alt. client certificate or key vault credential
+    },
+    authRoutes: {
+        redirect: "/redirect",
+        error: "/error", // the wrapper will redirect to this route in case of any error.
+        unauthorized: "/unauthorized" // the wrapper will redirect to this route in case of unauthorized access attempt.
+    }
+};
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
@@ -26,9 +40,39 @@ app.use(sessions({
     cookie: { maxAge: oneDay },
     resave: false 
 }))
+// instantiate the MS auth wrapper
+const msid = new MsIdExpress.WebAppAuthClientBuilder(appSettings).build();
+// initialize the MS auth wrapper
+app.use(msid.initialize());
+app.use( function (req, res, next) {
+    req.msid = msid;
+    next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// authentication routes
+app.get('/signin', 
+    msid.signIn({
+        postLoginRedirect: '/'
+    }
+));
+
+app.get('/signout', 
+    msid.signOut({
+        postLogoutRedirect: '/'
+    }
+));
+
+// unauthorized
+app.get('/error', (req, res) => res.status(500).send('server error'));
+
+// error
+app.get('/unauthorized', (req, res) => res.status(401).send('Permission denied'));
+
+
+
 
 export default app;
