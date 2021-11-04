f972adfe0f461a434a460cdf67288573ebad0a69
kyle thayer <kyle.thayer@gmail.com>
2021-11-03 23:16:24 -0700

    finish demo-1-sessions

--- 11 lecture/demo-1-sessions-finish/routes/users.js
+++ 11 lecture/demo-1-sessions-finish/routes/users.js
@@ -3,20 +3,39 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let session=req.session;
+  if(session.userid){
+    //todo: look up info for this userid
+    res.send("Here is your information, " + session.userid);
+  } else {
+    res.send('ERROR: you must login to see user information');
+  }
 });
 
 router.post("/login", function(req, res, next) {
+  if(req.session.userid){
+    res.send("You're already logged in as " + req.session.userid + ". Please log out before logging in again.")
+    return;
+  }
+
   //check username and password
-  if(req.body.username == "kylethayer" && req.body.password == "asdasd"){
-    //TODO: start a session
-    res.send("you logged in")
+  if(
+     (req.body.username == "kylethayer" && req.body.password == "asdasd") ||
+     (req.body.username == "anotheruser" && req.body.password == "pwd") ){
+    let session=req.session;
+    session.userid=req.body.username;
+    console.log(req.session)
+    res.send("you logged in as " + req.body.username + "!")
   } else{
     //not start session
     res.send("wrong login info")
   }
-
-  
 })
+
+router.post("/logout", function(req, res, next) {
+  req.session.destroy();
+  res.send("you are logged out!")
+})
+
 
 export default router;
