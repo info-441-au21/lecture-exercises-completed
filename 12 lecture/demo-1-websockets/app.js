afd3bdb14e7ca93569ffdf6db4ca258ae5e9e544
kyle thayer <kyle.thayer@gmail.com>
2021-11-09 11:16:00 -0800

    finish websockets demo

--- 12 lecture/demo-1-websockets/app.js
+++ 12 lecture/demo-1-websockets/app.js
@@ -1,12 +1,35 @@
 import fs from 'fs';
 import express from 'express';
+import enableWs from 'express-ws';
 
 const app = express();
+enableWs(app);
 
 app.get(['/', '/index.html'], (req, res) => {
   res.type('html')
   let cssContents = fs.readFileSync("index.html").toString();
   res.send(cssContents)
+})
+
+let allWebSockets = [];
+let socketCounter = 1;
+
+app.ws('/socket', (ws, req) => {
+
+  allWebSockets.push(ws);
+  let myNum = socketCounter;
+  socketCounter++;
+  
+  ws.on('message', msg => {
+    allWebSockets.forEach( socket => {
+      socket.send(myNum + ": " + msg);
+    })
+  })
+
+  ws.on('close', () => {
+    //TODO: remove it from the allWebSockets array
+    console.log("websocket " + myNum + " closed")
+  })
 })
 
 app.listen(3000, () => {
